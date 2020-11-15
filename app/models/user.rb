825e621fd9c94c722783af6e7f9d0f352112c900
uochann <ma602sa@gmail.com>
2020-11-15 19:53:47 +0900

    バリデーション名称変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validatable :name, presense: true
+  validates :name, presense: true
 end
