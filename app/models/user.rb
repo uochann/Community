3cc28497c1b69d01e6917b1de425c71427ffcc6e
uochann <ma602sa@gmail.com>
2020-11-16 18:53:04 +0900

    SnsCredential編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   validates :name, presence: true
 
+  has_many :sns_credentials
   has_many :room_users
   has_many :rooms, through: :room_users
   has_many :messages
