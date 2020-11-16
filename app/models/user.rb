0ac8bfa65f81234c2c315f25e8418aa0f2466f3c
uochann <ma602sa@gmail.com>
2020-11-16 19:30:37 +0900

    ビューファイル変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,20 @@ class User < ApplicationRecord
   has_many :room_users
   has_many :rooms, through: :room_users
   has_many :messages
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+       # sns認証したことがあればアソシエーションで取得
+   # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
