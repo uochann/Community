14ddd72b7432c25b7cb98c6ab6ef09e2f94dae77
uochann <ma602sa@gmail.com>
2020-11-16 19:53:08 +0900

    Google認証のビュー編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
        # sns認証したことがあればアソシエーションで取得
    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
     user = User.where(email: auth.info.email).first_or_initialize(
-      nickname: auth.info.name,
+        name: auth.info.name,
         email: auth.info.email
     )
     # userが登録済みであるか判断
