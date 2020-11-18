7d25a3dbcbd15febf4ff447227026bea02181b0f
uochann <ma602sa@gmail.com>
2020-11-18 11:36:28 +0900

    空のコミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,6 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
-       # sns認証したことがあればアソシエーションで取得
-   # 無ければemailでユーザー検索して取得orビルド(保存はしない)
     user = User.where(email: auth.info.email).first_or_initialize(
         name: auth.info.name,
         email: auth.info.email
