38cf1134e5b4d69f87a1f5f5247ea0105de17ef2
uochann <ma602sa@gmail.com>
2020-11-18 16:00:07 +0900

    JSONファイル導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
@@ -7,5 +8,6 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
+    resources :events
   end
 end
