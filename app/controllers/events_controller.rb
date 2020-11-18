ca491fcea6a4a33c5fdb8459939efd30cf6c9ad7
uochann <ma602sa@gmail.com>
2020-11-18 18:22:19 +0900

    モデルの作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,34 +1,34 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Events.all
+    @events = Event.all
   end
 
   def new
-    @event = Events.new
+    @event = Event.new
   end
 
   def show
-    @event = Events.find(params[:id])
+    @event = Event.find(params[:id])
   end
 
   def create
-    Events.create(event_parameter)
+    Event.create(event_parameter)
     redirect_to events_path
   end
 
   def destroy
-    @event = Events.find(params[:id])
+    @event = Event.find(params[:id])
     @event.destroy
     redirect_to events_path, notice:"削除しました"
   end
 
   def edit
-    @event = Events.find(params[:id])
+    @event = Event.find(params[:id])
   end
 
   def update
-    @event = Events.find(params[:id])
+    @event = Event.find(params[:id])
     if @event.update(event_parameter)
       redirect_to events_path, notice: "編集しました"
     else
