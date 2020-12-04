94503cbccf060d67de097b78179d62631a628fc6
uochann <ma602sa@gmail.com>
2020-12-04 14:25:27 +0900

    userモデルの単体テストコードを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,30 +13,30 @@ RSpec.describe User, type: :model do
     it "nameが空では登録できないこと" do
       @user.name = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("Name can't be blank")
+      expect(@user.errors.full_messages).to include("Nameを入力してください")
     end
 
     it "emailが空では登録できないこと" do
       @user.email = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
+      expect(@user.errors.full_messages).to include("Emailを入力してください")
     end
 
     it "passwordが空では登録できないこと" do
       @user.password = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
+      expect(@user.errors.full_messages).to include("Passwordを入力してください", "Passwordは不正な値です", "Password confirmationとPasswordの入力が一致しません")
     end
 
     it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
       @user.password_confirmation = ""
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      expect(@user.errors.full_messages).to include("Password confirmationとPasswordの入力が一致しません")
     end
 
     it "passwordが6文字以上であれば登録できること" do
-      @user.password = "123456"
-      @user.password_confirmation = "123456"
+      @user.password = "Masafly602"
+      @user.password_confirmation = "Masafly602"
       expect(@user).to be_valid
     end
 
@@ -44,14 +44,14 @@ RSpec.describe User, type: :model do
       @user.password = "12345"
       @user.password_confirmation = "12345"
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      expect(@user.errors.full_messages).to include("Passwordは6文字以上で入力してください")
     end
 
     it "重複したemailが存在する場合登録できないこと" do
       @user.save
       another_user = FactoryBot.build(:user, email: @user.email)
       another_user.valid?
-     expect(another_user.errors.full_messages).to include("Email has already been taken")
+     expect(another_user.errors.full_messages).to include("Emailはすでに存在します")
     end
   end
 end
