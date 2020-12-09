1e3e659dea1232fb954a408ce5806c430a2aeb46
uochann <ma602sa@gmail.com>
2020-12-09 13:53:14 +0900

    ヘルパメソッド定義

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_event, only: [:edit, :show, :update, :destroy]
   def index
     @events = Event.all
