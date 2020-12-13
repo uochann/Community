5b5f3cd04271f2b471a5c0b776c5225cb5dcedf8
uochann <ma602sa@gmail.com>
2020-12-13 21:07:09 +0900

    修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,16 +4,11 @@ class RoomsController < ApplicationController
   end
 
   def new
-    @room = Room.new
   end
 
   def create
-    @room = Room.new(room_params)
-    if @room.save
-      redirect_to root_path
-    else
-      render :new
-    end
+    Room.create(room_params)
+    redirect_to root_path notice: '作成しました'
   end
 
   def destroy
