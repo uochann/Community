d82cf40f1ec2d7185c76165177c946d70b68b4ae
uochann <ma602sa@gmail.com>
2020-12-14 18:46:22 +0900

    変更

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,5 @@ class Room < ApplicationRecord
   has_many :users, through: :room_users, dependent: :destroy
   has_many :messages, dependent: :destroy
 
-  validates :content, presence: true
+  validates :name, presence: true
 end
