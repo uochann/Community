e9759d1d8ac03331048c42d234ef16f6dfbbc99f
uochann <ma602sa@gmail.com>
2020-12-16 15:47:00 +0900

    修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -24,6 +24,6 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_id: [])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_073736) do
+ActiveRecord::Schema.define(version: 2020_12_16_064202) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
