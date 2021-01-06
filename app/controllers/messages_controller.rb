37cbaf2b083ae9a09c1c6d0cfec139a7e8f60487
uochann <ma602sa@gmail.com>
2021-01-06 19:43:31 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,10 +11,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      respond_to do |format|
-        format.html { redirect_to room_messages_path(@room) }
-        format.json
-      end
+      redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
       render :index
