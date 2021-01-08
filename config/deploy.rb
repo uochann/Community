ca951268d748a1170f1672dbeddf338433f34ca5
uochann <ma602sa@gmail.com>
2021-01-08 17:26:37 +0900

    キーの指定場所を変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,9 +14,11 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/uomasa.pem'] 
-
+set :ssh_options, {
+  forward_agent: true,
+  user: fetch(:user),
+  keys: ["#{ENV['PRODUCTION_SSH_KEY']}"]
+}
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
