54ea67033901782cf0228d4bb2ceeee8440a1803
uochann <ma602sa@gmail.com>
2021-01-15 15:56:40 +0900

    メッセージ機能非同期処理の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,10 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      redirect_to room_messages_path(@room)
+      respond_to do |format|
+        format.html { redirect_to room_messages_path(@room) }
+        format.json
+      end
     else
       @messages = @room.messages.includes(:user)
       render :index
