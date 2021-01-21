e6aa14d81f8f6c39c60fdbea62d0392e27613c8d
uochann <ma602sa@gmail.com>
2021-01-21 19:10:29 +0900

    自動テストエラー発生修正しました。

--- app/javascript/message.js
+++ app/javascript/message.js
@@ -1,55 +1,55 @@
 $(document).on('turbolinks:load', function(){
-  function buildHTML(message) {
-    var content = message.content ? `${ message.content }` : "";
-    var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message" data-id="${message.id}">
-                  <div class="upper-message">
-                    <p class="message-user">
-                      ${message.user_name}
-                    </p>
-                    <p class="message-date">
-                      ${message.date}
-                    </p>
-                  </div>
-                  <p class="lower-message">
-                    <div class="message-content">
-                    ${content}
+    function buildHTML(message) {
+      var content = message.content ? `${ message.content }` : "";
+      var img = message.image ? `<img src= ${ message.image }>` : "";
+      var html = `<div class="message" data-id="${message.id}">
+                    <div class="upper-message">
+                      <p class="message-user">
+                        ${message.user_name}
+                      </p>
+                      <p class="message-date">
+                        ${message.date}
+                      </p>
                     </div>
-                  </p>
-                </div>`
-  return html;
-  }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var message = new FormData(this);
-    var url = (window.location.href);
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: message,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+                    <p class="lower-message">
+                      <div class="message-content">
+                      ${content}
+                      </div>
+                    </p>
+                  </div>`
+    return html;
+    }
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var message = new FormData(this);
+      var url = (window.location.href);
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: message,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('#message_content').val('');
+        scrollBottom();
+      })
+      .fail(function(data){
+        alert('エラーが発生したためメッセージは送信できませんでした。');
+      })
+      .always(function(data){
+        $('.form-submit').prop('disabled', false);
+      })
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('#message_content').val('');
-      scrollBottom();
-    })
-    .fail(function(data){
-      alert('エラーが発生したためメッセージは送信できませんでした。');
-    })
-    .always(function(data){
-      $('.form-submit').prop('disabled', false);
-    })
-  })
-});
+  });
 
-function scrollBottom(){
-  var target = $('.message').last();
-  var position = target.offset().top + $('.messages').scrollTop();
-  $('.messages').animate({
-    scrollTop: position
-  }, 300, 'swing');
-}
+  function scrollBottom(){
+    var target = $('.message').last();
+    var position = target.offset().top + $('.messages').scrollTop();
+    $('.messages').animate({
+      scrollTop: position
+    }, 300, 'swing');
+  }
