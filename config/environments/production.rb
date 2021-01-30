f2104df0648b79a1a582a00abb0ee45d1f441244
uochann <ma602sa@gmail.com>
2021-01-30 16:13:17 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'https://d2xhdhqkre623x.cloudfront.net'
+  # config.action_controller.asset_host = '//d2xhdhqkre623x.cloudfront.net'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
